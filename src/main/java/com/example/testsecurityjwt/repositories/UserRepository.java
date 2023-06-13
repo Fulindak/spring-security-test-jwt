77560c3fdb04095503441aa92691760ec4c15c60
Кузьмин Данила Андреевич <200120@edu.psuti.ru>
2023-06-13 15:31:39 +0300

    Базовые настройки и утилитарный клас для jwt

---
+++ src/main/java/com/example/testsecurityjwt/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.testsecurityjwt.repositories;
+
+import com.example.testsecurityjwt.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByUserName(String userName);
+}
