f0500d0afe3f9bb424a2c15395d56ecf819d75dc
Кузьмин Данила Андреевич <200120@edu.psuti.ru>
2023-07-01 14:44:31 +0300

    fix UserService

---
+++ src/main/java/com/example/testsecurityjwt/service/UserService.java
@@ -0,0 +1,67 @@
+package com.example.testsecurityjwt.service;
+
+import com.example.testsecurityjwt.dto.RegistrationUserDto;
+import com.example.testsecurityjwt.entitie.User;
+import com.example.testsecurityjwt.repositorie.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService implements UserDetailsService {
+    private UserRepository userRepository;
+    private  RoleService roleService;
+    private PasswordEncoder passwordEncoder;
+
+    public Optional<User> findUserByUserName(String username) {
+        return userRepository.findByUserName(username);
+    }
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Autowired
+    public void setRoleSevice(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @Autowired
+    public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = findUserByUserName(username).orElseThrow(() -> new UsernameNotFoundException(
+                String.format("User: '%s' not found", username)
+        ));
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                user.getRoles()
+                        .stream()
+                        .map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList())
+        );
+    }
+
+    public User createNewUser(RegistrationUserDto registrationUserDto) {
+        User user = new User();
+        user.setUsername(registrationUserDto.getUsername());
+        user.setEmail(registrationUserDto.getEmail());
+        user.setPassword(passwordEncoder.encode(registrationUserDto.getPassword()));
+        user.setRoles(List.of(roleService.getUserRole()));
+        return userRepository.save(user);
+    }
+}
