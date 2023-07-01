8c8a1f4e7db642f24ec31484f81d8e1e88fa0d17
Кузьмин Данила Андреевич <200120@edu.psuti.ru>
2023-07-01 14:45:36 +0300

    fix UserService

--- src/main/java/com/example/testsecurityjwt/service/UserService.java
+++ src/main/java/com/example/testsecurityjwt/service/UserService.java
@@ -32,7 +32,7 @@ public class UserService implements UserDetailsService {
     }
 
     @Autowired
-    public void setRoleSevice(RoleService roleService) {
+    public void setRoleService(RoleService roleService) {
         this.roleService = roleService;
     }
 
