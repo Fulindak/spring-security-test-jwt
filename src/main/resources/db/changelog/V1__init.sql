cb7d3988cfc005ee16bc44e242e175876e5c78b9
Кузьмин Данила Андреевич <200120@edu.psuti.ru>
2023-07-01 14:49:35 +0300

    sql migration

--- src/main/resources/db/changelog/V1__init.sql
+++ src/main/resources/db/changelog/V1__init.sql
@@ -1,36 +1,35 @@
 create table users (
-    id              bigserial,
-    username        varchar(50) not null  unique,
-    password        varchar(80) not null,
-    email           varchar(50) unique,
-    primary key (id)
+                       id                    bigserial,
+                       username              varchar(30) not null unique,
+                       password              varchar(80) not null,
+                       email                 varchar(50) unique,
+                       primary key (id)
 );
 
-create  table  roles (
-    id              serial,
-    name            varchar(50) not null,
-    primary key (id)
+create table roles (
+                       id                    serial,
+                       name                  varchar(50) not null,
+                       primary key (id)
 );
 
-create table users_roles (
-    user_id         bigint not null,
-    role_id         int not null,
-    primary key (user_id, role_id),
-    foreign key (user_id) references  users(id),
-    foreign key (role_id) references  roles(id)
+CREATE TABLE users_roles (
+                             user_id               bigint not null,
+                             role_id               int not null,
+                             primary key (user_id, role_id),
+                             foreign key (user_id) references users (id),
+                             foreign key (role_id) references roles (id)
 );
 
 insert into roles (name)
 values
-    ('ROLE_ADMIN'),
-    ('ROLE_USER');
+    ('ROLE_USER'), ('ROLE_ADMIN');
 
 insert into users (username, password, email)
 values
-    ('user', '$2a$10$pTPdDB9Bn8Pnz/7TTkY/WewqKoGkaqq4CxSYSfmOxT/VU/l5pyZBi', 'user@yandex.ru'),
-    ('admin', '$2a$10$pTPdDB9Bn8Pnz/7TTkY/WewqKoGkaqq4CxSYSfmOxT/VU/l5pyZBi', 'admin@yandex.ru');
+    ('user', '$2a$04$Fx/SX9.BAvtPlMyIIqqFx.hLY2Xp8nnhpzvEEVINvVpwIPbA3v/.i', 'user@gmail.com'),
+    ('admin', '$2a$04$Fx/SX9.BAvtPlMyIIqqFx.hLY2Xp8nnhpzvEEVINvVpwIPbA3v/.i', 'admin@gmail.com');
 
-insert into users_roles(user_id, role_id)
+insert into users_roles (user_id, role_id)
 values
-    (1, 2),
-    (2, 1);
+    (1, 1),
+    (2, 2);
