cdb82c2d15dc75bf1f69b5e25931e547ec562cb1
Кузьмин Данила Андреевич <200120@edu.psuti.ru>
2023-07-01 14:44:02 +0300

    add UserDTO and RoleService

---
+++ src/main/java/com/example/testsecurityjwt/service/RoleService.java
@@ -0,0 +1,16 @@
+package com.example.testsecurityjwt.service;
+
+import com.example.testsecurityjwt.entitie.Role;
+import com.example.testsecurityjwt.repositorie.RoleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RoleService {
+    private final RoleRepository roleRepository;
+
+    public Role getUserRole() {
+        return roleRepository.findByName("ROLE_USER").get();
+    }
+}
